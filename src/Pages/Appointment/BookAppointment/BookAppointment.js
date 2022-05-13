407877c671a10c35086904140ad4fcf4f7474fc0
forhadmia1 <forhad8613@gmail.com>
2022-05-14 01:34:46 +0600

    use react query to load data

--- src/Pages/Appointment/BookAppointment/BookAppointment.js
+++ src/Pages/Appointment/BookAppointment/BookAppointment.js
@@ -2,21 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import Service from '../Service/Service';
 import BookingModal from '../BookingModal/BookingModal';
+import { useQuery } from 'react-query';
+import Loading from '../../Shared/Loading';
 
 const BookAppointment = ({ date }) => {
-    const [services, setServices] = useState([])
     const [treatment, setTreatment] = useState(null)
-    useEffect(() => {
-        fetch('http://localhost:5000/service')
+    const formatDate = format(date, 'PP');
+
+    const { isLoading, data: services, refetch } = useQuery(['available', formatDate], () =>
+        fetch(`http://localhost:5000/available?date=${formatDate}`)
             .then(res => res.json())
-            .then(data => setServices(data))
-    }, [])
+    )
+
+    if (isLoading) {
+        return <Loading />
+    }
+
     return (
         <section className='container mx-auto'>
             <h2 className='text-center text-secondary text-xl font-semibold'>Available appointments on {format(date, 'PP')}</h2>
             <div className='grid grid-cols-1 md:grid-cols-3 gap-5 my-24'>
                 {
-                    services.map(service => <Service
+                    services?.map(service => <Service
                         service={service}
                         key={service._id}
                         setTreatment={setTreatment}
@@ -27,6 +34,7 @@ const BookAppointment = ({ date }) => {
                 date={date}
                 treatment={treatment}
                 setTreatment={setTreatment}
+                refetch={refetch}
             />}
         </section>
     );
--- src/Pages/Appointment/BookingModal/BookingModal.js
+++ src/Pages/Appointment/BookingModal/BookingModal.js
@@ -2,18 +2,40 @@ import React from 'react';
 import { format } from 'date-fns';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../../firebase.init';
+import { toast } from 'react-toastify';
 
-const BookingModal = ({ treatment, date, setTreatment }) => {
+const BookingModal = ({ treatment, date, setTreatment, refetch }) => {
     const [user] = useAuthState(auth)
     const { name, slots } = treatment;
+    const formatDate = format(date, 'PP');
     const handleModalForm = (e) => {
         e.preventDefault()
         const slot = e.target.slot.value;
-        const name = e.target.name.value;
-        const email = e.target.email.value;
+        const patient = user.displayName;
+        const email = user.email;
         const phone = e.target.phone.value;
-        console.log(slot, name, date, email, phone)
-        setTreatment(null)
+        const booking = {
+            treatment: name, patient, email, phone, date: formatDate, slot
+        }
+
+        fetch('http://localhost:5000/booking', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.success) {
+                    toast.success('Appointment is saved successfully')
+                }
+                else {
+                    toast.error(`Already have an appointment on ${data.exist.date} at ${data.exist.slot}`)
+                }
+                refetch()
+                setTreatment(null)
+            })
 
     }
     return (
@@ -25,10 +47,12 @@ const BookingModal = ({ treatment, date, setTreatment }) => {
                     <h3 className="font-bold text-lg">{name}</h3>
                     <div className='mt-4'>
                         <form onSubmit={handleModalForm} className='grid grid-cols-1 justify-items-center gap-4'>
-                            <input type="text" value={format(date, 'PP')} disabled className="input input-bordered w-full max-w-xs" />
+                            <input type="text" value={formatDate} disabled className="input input-bordered w-full max-w-xs" />
                             <select name='slot' className="select select-bordered w-full max-w-xs">
                                 {
-                                    slots.map(slot => <option>{slot}</option>)
+                                    slots.map(slot => <option
+                                        key={slot._id}
+                                    >{slot}</option>)
                                 }
                             </select>
                             <input type="text" value={user?.displayName} className="input input-bordered w-full max-w-xs" disabled />
